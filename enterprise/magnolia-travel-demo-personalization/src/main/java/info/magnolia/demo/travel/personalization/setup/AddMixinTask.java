09d72bdc0233437f74d79f6b50f464b1348fcc28
Christopher Zimmermann <christopher.zimmermann@magnolia-cms.com>
2015-05-13 22:07:46 +0200

    MGNLDEMO-14 Add personalization features to the travel demo.

---
+++ enterprise/magnolia-travel-demo-personalization/src/main/java/info/magnolia/demo/travel/personalization/setup/AddMixinTask.java
@@ -0,0 +1,56 @@
+/**
+ * This file Copyright (c) 2015 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.demo.travel.personalization.setup;
+
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.AbstractRepositoryTask;
+import info.magnolia.module.delta.TaskExecutionException;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+/**
+ * Adds a mixin to a node.
+ */
+public class AddMixinTask extends AbstractRepositoryTask {
+
+    private final String nodePath;
+    private final String workspace;
+    private final String newMixin;
+
+    /**
+     * @param nodePath the path to the node
+     * @param workspace the workspace where the node is stored
+     * @param newMixin the full name of the mixin to add
+     */
+    public AddMixinTask(String nodePath, String workspace, String newMixin) {
+        super("Add mixin to " + nodePath, "Add " + newMixin + " mixin to " + nodePath);
+        this.nodePath = nodePath;
+        this.workspace = workspace;
+        this.newMixin = newMixin;
+    }
+
+    @Override
+    protected void doExecute(InstallContext ctx) throws RepositoryException, TaskExecutionException {
+        final Session session = ctx.getJCRSession(workspace);
+        try {
+            final Node node = session.getNode(nodePath);
+            node.addMixin(newMixin);
+        } catch (RepositoryException e) {
+            ctx.error(String.format("Could not add new mixin %s to %s", newMixin, nodePath), e);
+        }
+    }
+}
---
+++ enterprise/magnolia-travel-demo-personalization/src/main/java/info/magnolia/demo/travel/personalization/setup/TravelDemoPersonalizationModuleVersionHandler.java
@@ -0,0 +1,45 @@
+/**
+ * This file Copyright (c) 2015 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.demo.travel.personalization.setup;
+
+import info.magnolia.module.DefaultModuleVersionHandler;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.OrderNodeToFirstPositionTask;
+import info.magnolia.module.delta.Task;
+import info.magnolia.personalization.variant.VariantManager;
+import info.magnolia.repository.RepositoryConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link DefaultModuleVersionHandler} for travel-demo personalization module.
+ */
+public class TravelDemoPersonalizationModuleVersionHandler extends DefaultModuleVersionHandler {
+
+    @Override
+    protected List<Task> getExtraInstallTasks(InstallContext installContext) {
+        final List<Task> tasks = new ArrayList<Task>();
+
+        tasks.addAll(super.getExtraInstallTasks(installContext));
+
+        // Add variant mixin to travel page - so that adminCentral gives it the proper behaviour.
+        tasks.add(new AddMixinTask("/travel", RepositoryConstants.WEBSITE, VariantManager.HAS_VARIANT_MIXIN));
+        tasks.add(new OrderNodeToFirstPositionTask("Order travel page variants to first position.", "", RepositoryConstants.WEBSITE, "travel/variants"));
+
+        return tasks;
+    }
+
+}
