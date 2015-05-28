901baaaf931f086ffc0f9c69e8099dc2ba195b3d
Philip Mundt <philip.mundt@magnolia-cms.com>
2015-05-28 08:19:18 +0200

    QA Remove default javadoc of MVH

--- community/magnolia-travel-tours/src/main/java/info/magnolia/demo/travel/tours/setup/ToursModuleVersionHandler.java
+++ community/magnolia-travel-tours/src/main/java/info/magnolia/demo/travel/tours/setup/ToursModuleVersionHandler.java
@@ -47,9 +47,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class is optional and lets you manager the versions of your module,
- * by registering "deltas" to maintain the module's configuration, or other type of content.
- * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * {@link DefaultModuleVersionHandler} of the {@link info.magnolia.demo.travel.tours.ToursModule}.
  */
 public class ToursModuleVersionHandler extends DefaultModuleVersionHandler {
 
