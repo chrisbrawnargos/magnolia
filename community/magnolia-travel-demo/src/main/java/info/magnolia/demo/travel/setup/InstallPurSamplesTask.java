820ce270fcd8ffa4c7d8a80cf1e3619392947e90
rkovarik <roman.kovarik@magnolia-cms.com>
2015-11-11 15:01:26 +0100

    MGNLDEMO-104 add demo of PUR module
    Conflicts:
    	community/magnolia-travel-demo/src/main/java/info/magnolia/demo/travel/setup/TravelDemoModuleVersionHandler.java
    	community/magnolia-travel-demo/src/test/java/info/magnolia/demo/travel/setup/TravelDemoModuleVersionHandlerTest.java
    
    Conflicts:
    	community/magnolia-travel-demo/src/main/java/info/magnolia/demo/travel/setup/TravelDemoModuleVersionHandler.java
    	community/magnolia-travel-demo/src/main/resources/info/magnolia/module/travel-demo/setup/public-user-registration/config.server.filters.securityCallback.clientCallbacks.travel-demo-pur.xml
    	community/magnolia-travel-demo/src/main/resources/info/magnolia/module/travel-demo/setup/public-user-registration/userroles.travel-demo-pur.xml
    	community/magnolia-travel-demo/src/main/resources/travel-demo/templates/pages/pur.yaml
    	community/magnolia-travel-demo/src/test/java/info/magnolia/demo/travel/setup/TravelDemoModuleVersionHandlerTest.java

---
+++ community/magnolia-travel-demo/src/main/java/info/magnolia/demo/travel/setup/InstallPurSamplesTask.java
@@ -0,0 +1,85 @@
+/**
+ * This file Copyright (c) 2015 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.demo.travel.setup;
+
+import info.magnolia.cms.security.UserManager;
+import info.magnolia.module.delta.AddURIPermissionTask;
+import info.magnolia.module.delta.ArrayDelegateTask;
+import info.magnolia.module.delta.IsAuthorInstanceDelegateTask;
+import info.magnolia.module.delta.IsModuleInstalledOrRegistered;
+import info.magnolia.module.delta.ModuleDependencyBootstrapTask;
+import info.magnolia.module.delta.OrderNodeBeforeTask;
+import info.magnolia.module.delta.Task;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * {@link ArrayDelegateTask} which install samples for Public User Registration.
+ */
+public class InstallPurSamplesTask extends ArrayDelegateTask {
+
+    protected static final String PUR_SAMPLE_ROOT_PAGE_NAME = "members";
+
+    protected static final List<String> PROTECTED_PAGES_NAMES = Arrays.asList(
+            "protected",
+            "profile-update"
+    );
+
+    protected static final List<String> PROTECTED_PAGES_PATHS = Arrays.asList(
+            "/travel/" + PUR_SAMPLE_ROOT_PAGE_NAME + "/" + PROTECTED_PAGES_NAMES.get(0) + "*",
+            "/travel/" + PUR_SAMPLE_ROOT_PAGE_NAME + "/"  + PROTECTED_PAGES_NAMES.get(1) +  "*",
+            "<travel>/" + PUR_SAMPLE_ROOT_PAGE_NAME + "/"  + PROTECTED_PAGES_NAMES.get(0) +  "*",
+            "<travel>/" + PUR_SAMPLE_ROOT_PAGE_NAME + "/"  + PROTECTED_PAGES_NAMES.get(1) + "*"
+    );
+
+    protected static final String PASSWORD_CHANGE_PAGE_PATH = "travel/" + PUR_SAMPLE_ROOT_PAGE_NAME + "/forgotten-password/password-change";
+
+    public InstallPurSamplesTask() {
+        super("Install PUR samples if public-user-registration module is installed");
+        this.addTask(new ModuleDependencyBootstrapTask("public-user-registration"));
+        this.addTask(new IsModuleInstalledOrRegistered("", "public-user-registration", new ArrayDelegateTask("",
+                new IsAuthorInstanceDelegateTask("", (Task) null, this.getPermissionTasks()),
+                new OrderNodeBeforeTask("/server/filters/securityCallback/clientCallbacks/travel-demo-pur", "form")
+        )));
+    }
+
+    private ArrayDelegateTask getPermissionTasks() {
+        ArrayDelegateTask task = new ArrayDelegateTask("");
+        for (String page : PROTECTED_PAGES_PATHS) {
+            task.addTask(new AddURIPermissionTask("", UserManager.ANONYMOUS_USER, page, AddURIPermissionTask.DENY));
+        }
+        return task;
+    }
+}
