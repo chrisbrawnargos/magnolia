3727f7d8f4ef3c7a4e906be26680edc92357307b
Philip Mundt <philip.mundt@magnolia-cms.com>
2016-08-25 16:42:01 +0200

    QA Bump MVH of all submodules to 0.14

--- enterprise/magnolia-travel-demo-personalization/src/main/java/info/magnolia/demo/travel/personalization/setup/TravelDemoPersonalizationModuleVersionHandler.java
+++ enterprise/magnolia-travel-demo-personalization/src/main/java/info/magnolia/demo/travel/personalization/setup/TravelDemoPersonalizationModuleVersionHandler.java
@@ -40,7 +40,7 @@ public class TravelDemoPersonalizationModuleVersionHandler extends DefaultModule
     private final Task orderVariantNodeToFirstPosition = new OrderNodeToFirstPositionTask("Order travel page variants to first position.", "", RepositoryConstants.WEBSITE, "travel/variants");
 
     public TravelDemoPersonalizationModuleVersionHandler() {
-        register(DeltaBuilder.update("0.13", "")
+        register(DeltaBuilder.update("0.14", "")
                 .addTask(new IsInstallSamplesTask("Re-Bootstrap website variants for travel pages", "Re-bootstrap website variants to account for all changes",
                         new BootstrapSingleResource("Re-Bootstrap variants", "", "/mgnl-bootstrap-samples/travel-demo-personalization/website.travel.variants.xml", ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING)))
                 .addTask(new AddPermissionTask("Add permission", "travel-demo-admincentral", "personas", "/*", Permission.READ, true))
